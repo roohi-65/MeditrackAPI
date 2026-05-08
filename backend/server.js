83ff784d79e4373908378a44c1193b3ef4658495
Mowfika Roohi <mowfikaroohi@gmail.com>
2026-05-08 12:52:43 +0530

    Initial project setup - folder structure and dependencies

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+//app.get('/', (req, res) => res.json({ message: 'MediTrack API Running ✅' }));
+
+// Connect DB and start server
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('MongoDB Connected ✅');
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    );
+  })
+  .catch(err => console.error(err));
